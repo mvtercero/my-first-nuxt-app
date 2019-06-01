29678efd66ab309ea3c5461e54e6a3dc99322501
María Vedia <mvtercero85@gmail.com>
2019-06-01 18:15:40 +0200

    (folder structure): change and create new files in pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,10 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        my-first-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My wonderful Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
+      <h1 class="title">My first Nuxt app</h1>
     </div>
   </section>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
 <style>
 .container {
   margin: 0 auto;
@@ -51,17 +23,5 @@ export default {
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
