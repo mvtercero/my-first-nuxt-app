4b8118b0fe2cc6f51727f04ccaad0167b0a5401a
María Vedia <mvtercero85@gmail.com>
2019-06-01 20:49:37 +0200

    validate parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,17 @@
 <template>
   <h1>A single user with id: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id)
+  }
+
+  //  validate(data) {
+  //   // Must be a number
+  //   return /^\d+$/.test(data.params.id)
+  // }
+}
+</script>
